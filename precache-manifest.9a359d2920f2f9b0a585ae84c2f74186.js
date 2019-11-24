8553a2f0cb0c333f0d6cac9e86a504bec05a93c8
clasgit <cyril@lassalle.cloud>
2019-11-24 14:51:53 +0100

    init commit (First vue)

---
+++ precache-manifest.9a359d2920f2f9b0a585ae84c2f74186.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8392d17a9c9e6b30038",
+    "url": "/css/app.047b401c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d635784cd4a563584843e90d5745cc04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6601a06038ffd9a5ff2",
+    "url": "/js/about.af64f31f.js"
+  },
+  {
+    "revision": "a8392d17a9c9e6b30038",
+    "url": "/js/app.4a60f121.js"
+  },
+  {
+    "revision": "217bd1bc8b02ef3cbac7",
+    "url": "/js/chunk-vendors.2202b159.js"
+  },
+  {
+    "revision": "65d2ed53fa778166f66e436ddf0632d3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
